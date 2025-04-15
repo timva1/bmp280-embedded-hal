85980cab35242ac7f2465fab4c439e9919965bb2
timva1 <timvanantwerpen@icloud.com>
2025-04-15 18:06:52 -0400

    Add mbp280 drivers for old embedded hal version

---
+++ examples/rp2040/src/main.rs
@@ -0,0 +1,36 @@
+#![no_std]
+#![no_main]
+
+use {defmt_rtt as _, panic_probe as _};
+use embassy_rp::{
+    self,
+    i2c::Config,
+};
+use bmp280_embedded_hal::BMP280;
+
+#[cortex_m_rt::entry]
+fn main() -> ! {
+    defmt::println!("Hello, world!");
+
+    let p = embassy_rp::init(Default::default());
+
+    let scl = p.PIN_15;
+    let sda = p.PIN_14;
+
+    let i2c = embassy_rp::i2c::I2c::new_blocking(p.I2C1, scl, sda, Config::default());
+
+    let mut bmp280 = BMP280::new(i2c).unwrap();
+
+    loop {
+        let temperature = bmp280.temp();
+        let pressure = bmp280.pressure();
+        let altitude = bmp280.altitude();
+
+        defmt::info!("Temp: {} C", temperature);
+        defmt::info!("Pressure: {} Pa", pressure);
+        defmt::info!("Altitude: {} m", altitude);
+
+        cortex_m::asm::delay(1_000_000);
+    }
+    
+}
